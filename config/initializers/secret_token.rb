ba7810e4acbf4a223064b4394fdd9d6314fd2d0c
skimtran@gmail.com <Kim@Kims-MacBook.local>
2012-03-24 23:51:29 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HellowWorld::Application.config.secret_token = '114a666e78f6ee0a85a9e79675a64b104f40e4e8bf7f287f996afe7f74a345842f893abf1db4b70a685eca33acf5fcded2898c97e84cf70ec4b4cece797dd019'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HellowWorld::Application.config.session_store :cookie_store, :key => '_HellowWorld_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HellowWorld::Application.config.session_store :active_record_store
